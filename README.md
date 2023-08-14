df26828030db755cec940439264bd272dfbebf6b
Mario Krutzikowsky <mario.krutzikowsky@gmail.com>
2023-08-14 12:42:59 +0200

    Update

--- README.md
+++ README.md
@@ -38,5 +38,17 @@ App Screens
       <p>Aufgabe 4</p>
     </td>
  </tr>
+ <tr>
+    <td style="text-align: center;">
+      <img src="lib/task_solutions/5_1_1_Styling/Simulator Screenshot - iPhone 13 mini - 2023-08-14 at 12.41.28.png" alt="Bild 4">
+      <p>Animated Button</p>
+    </td>
+ </tr>
+ <tr>
+    <td style="text-align: center;">
+      <img src="lib/task_solutions/5_1_1_Styling/Simulator Screenshot - iPhone 13 mini - 2023-08-14 at 12.41.30.png" alt="Bild 4">
+      <p>Animated Button</p>
+    </td>
+ </tr>
 
 </table>
---
+++ lib/task_solutions/5_1_1_Styling/aufgabe_5_2.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedButton(),
+    );
+  }
+}
+
+class AnimatedButton extends StatefulWidget {
+  const AnimatedButton({super.key});
+
+  @override
+  _AnimatedButtonState createState() => _AnimatedButtonState();
+}
+
+class _AnimatedButtonState extends State<AnimatedButton> {
+  bool _isClicked = false;
+
+  void _toggleClick() {
+    setState(() {
+      _isClicked = !_isClicked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Animierter Textbutton',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: _toggleClick,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 50),
+            width: _isClicked ? 150 : 200,
+            height: 50,
+            decoration: BoxDecoration(
+              color: _isClicked ? Colors.black : Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
+              child: Text(
+                'Klick mich!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
